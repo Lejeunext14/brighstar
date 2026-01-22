fe74bc81a4bb77a453cee8d277843e56f4319faa
jeckcastro <castrojeck34@gmail.com>
2026-01-23 04:21:21 +0800

    version2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class DatabaseSeeder extends Seeder
         'password' => Hash::make('Admin123456'),
         'role' => 'admin',
         'email_verified_at' => now(),
-        
+
     ]);
 }
 }
